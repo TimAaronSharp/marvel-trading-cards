ff5656f355e946c9c9eedc0aeb9d5cd14bf45725
Tim Sharp <timothyaaron.sharp@gmail.com>
2017-11-02 17:51:18 -0600

    can add to roster and it populates

--- cards-controller.js
+++ cards-controller.js
@@ -1,36 +1,56 @@
 function CardsController() {
-var marvelService = new globals.MarvelService()
+  var marvelService = new MarvelService()
 
-  this.add = function add(id){
-    console.log('characterId', id)
+  this.add = function add(id) {
+    marvelService.addToMyCharacters(id)
+    updateRoster()
   }
-
   marvelService.getCharacters(ready)
-
   function ready(data) {
 
     updateMarvel(data)
+  }
 
-    function updateMarvel(list) {
-      var elem = document.getElementById('marvel-characters')
-      elem.innerHTML = ''
-      var marvelTemplate = ''
-      for (var i in list) {
-        var character = list[i];
-        character.thumbnail.path = character.thumbnail.path.replace('http:', '')
-        marvelTemplate += `
-      <div class="card">
+  function updateMarvel(list) {
+    var elem = document.getElementById('marvel-characters')
+    elem.innerHTML = ''
+    var marvelTemplate = ''
+    for (var i in list) {
+      var character = list[i];
+      character.thumbnail.path = character.thumbnail.path.replace('http:', '')
+      marvelTemplate += `
+      <div class="col-md-3 card outline">
         <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
         <h3>${character.name}</h3>
-        <div>
-          <button class="btn-success" id="${character.id}" onclick="app.controllers.marvelController.add('${character.id}')">Add to Team</button>
-        </div>
-      <div>
+        
+          <button class="btn-success" id="${character.id}" onclick="app.controllers.cardsController.add('${character.id}')">Add to Team</button>
+        
+      </div>
       `
 
-        elem.innerHTML = marvelTemplate
-      }
-
+      elem.innerHTML = marvelTemplate
     }
-
+    console.log(marvelService.marvelCharacters)
+    console.log(marvelService.myCharacters)
+  }
+  function updateRoster() {
+    var myChars = marvelService.getMyCharacters()
+    var myElem = document.getElementById('my-characters')
+    myElem.innerHTML = ''
+    var myTemplate = ''
+    for (var i in myChars) {
+      var myChar = myChars[i];
+      myTemplate += `
+      <div class="col-md-2 roster-card outline">
+        <img src="${myChar.thumbnail.path}.${myChar.thumbnail.extension}" width="100">
+        <h3>${myChar.name}</h3>
+      
+         <button class="btn-success" id="${myChar.id}" onclick="app.controllers.cardsController.add('${myChar.id}')">Add to Team</button>
+      
+    </div>
+      `
+    }
+    myElem.innerHTML = myTemplate
+  }
 }
+
--- marvel-service.js
+++ marvel-service.js
@@ -1,37 +1,48 @@
-function MarvelService(){
-  var key = '?apikey=e44062bbc76b37176b08325d5265a0f3';
+function MarvelService() {
+  var key = '?apikey=51b95417c37ece75b754218134468561';
   var baseUrl = 'http://gateway.marvel.com/v1/public/'
-  
+
   var marvelCharacters = [];
   var myCharacters = [];
-  
-  
-  this.getMarvelCharacters = function(){
+
+
+  this.getMarvelCharacters = function (id) {
     //what should this function return
+    return JSON.parse(JSON.stringify(marvelCharacters))
   }
-  
-  this.getMyCharacters = function(){
+
+  this.getMyCharacters = function getMyCharacters(id) {
     //what should this function return
+    return JSON.parse(JSON.stringify(myCharacters))
+           
   }
-  
-  this.addToMyCharacters = function(id){
+
+  this.addToMyCharacters = function addToMyCharacters(id) {
     //in order to add a character to your list you will first need to find 
     //the character by its id in the marvelCharacters array
+    for (var i = 0; i < marvelCharacters.length; i++) {
+      if (marvelCharacters[i].id == id) {
+        myCharacters.push(marvelCharacters[i]) //ADDS CHAR TO MYCHARACTERS ARRAY
+        marvelCharacters.splice(marvelCharacters[i], 1) //REMOVES CHAR FROM MARVELCHARACTERS ARRAY. MAKES IT SO THEY CAN'T ADD MORE THAN ONE OF THE SAME CHAR, BUT PROBABLY WON'T WORK FOR                                                      MUTLI-TEAM BONUS CHALLENGE. MIGHT NEED TO REVISIT.
+      }
+    }
+    console.log(myCharacters)
+    console.log(marvelCharacters)
+    return JSON.parse(JSON.stringify(myCharacters))
   }
-  
-  this.removeMyCharacter = function(id){
+
+  this.removeMyCharacter = function removeMyCharacter(id) {
     //you need to find the character that you want to remove by its id
     //and remove it.
   }
-  
-  
-  this.getCharacters = function(callWhenDone){
+
+
+  this.getCharacters = function (callWhenDone) {
     //Use &offset=Number to add pagination
-    $.get(baseUrl + 'characters'+key, function(response){
-      marvelCharacters = response.data.results;
+    $.get(baseUrl + 'characters' + key, function (response) {
+      marvelCharacters = response.data.results; //THIS IS THE OBJECT PATH IN THE API TO THE OBJECT THAT HOLDS ALL THE CHARACTER OBJECTS.
       callWhenDone(marvelCharacters)
+      console.log(marvelCharacters)
     })
   }
-  
-  
 }
